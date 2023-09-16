647f17d11f6fbc9e04a925862487411e93baa1c4
PedroRuizS <pedroruizescuela@gmail.com>
2023-09-16 00:36:11 -0500

    un codigo para afk tonto en roblox

---
+++ codigos varios/powerjump.py
@@ -0,0 +1,20 @@
+#codigo para poder estar farmeando afk en un juego tonto de roblox
+import time
+import ctypes
+import pynput
+import time
+from keys import *
+
+print("YAAAA")
+countdown = 5
+inicio = input("Ingresa cuando quieres que empiece: ")
+if inicio == "ya":
+    while countdown > 0:
+        print(countdown)
+        countdown -= 1
+        time.sleep(1)
+    while True:
+        HoldKey(SPACE)
+        HoldAndReleaseKey(W, .5)
+        time.sleep(5)
+
